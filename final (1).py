2f19bfa591b2b628b72ecd0376214ce7dd503267
avivwei96 <76816214+avivwei96@users.noreply.github.com>
2024-02-25 14:11:09 +0200

    best results

--- final (1).py
+++ final (1).py
@@ -43,15 +43,19 @@
 
 
 import numpy as np
-from sklearn.compose import ColumnTransformer
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import RFECV
+from sklearn.linear_model import LogisticRegression
+from sklearn.base import BaseEstimator, ClassifierMixin
+
 
 
 def load_dataset(train_csv_path):
@@ -84,9 +88,18 @@ class DataPreprocessor(object):
     """
 
     def __init__(self):
-      self._resident_distance_regressor = None
-      self._season_classifier = None
-      self._age_mapper = None
+        self._resident_distance_regressor = None
+        self._weight_regressor = None
+        self._season_classifier = None
+        self._height = None
+        self._Service_time = None
+        self._time_off_mapper = self.TimeOff_maper()
+        self._age_mapper = self.get_age_group_mapper()
+        self._yes_no_mapper = self.get_drinker_or_smoker_mapper()
+        self._age_group_classifier = None
+        self._son_regressor = None
+        self._pet_regressor = None
+
 
     def data_visualing(self, df):
         """
@@ -111,7 +124,7 @@ class DataPreprocessor(object):
         None
         """
         # print age group uniqu
-        print(df['Age Group'].unique())
+        print(df['Smoker'].unique())
 
         # Create subplots
         fig, axes = plt.subplots(1, 2, figsize=(12, 6))
@@ -130,9 +143,11 @@ class DataPreprocessor(object):
         print(df.info())
         print('\n')
         print(df.describe())
-            
+
+
     def create_linear_regressor(self, X, y):
         # Prepare the training data
+        X = X.fillna(X.mean())
         df = pd.concat([X, y], axis=1)
         df_non_missing = df.dropna()
 
@@ -151,11 +166,14 @@ class DataPreprocessor(object):
     
 
     def create_SVM_classifier(self, X, y, kernel='linear'):
-        # Concatenate X and y to ensure alignment of rows
-        df = pd.concat([X, y], axis=1)
-        
-        # Drop rows with missing values
-        df_non_missing = df.dropna()
+        # Fill missing values in X with the median for each column
+        X_filled = X.fillna(X.mean())
+
+        # Ensure that y is aligned with X
+        df = pd.concat([X_filled, y], axis=1)
+
+        # Drop rows with missing values in y (if any)
+        df_non_missing = df.dropna(subset=[y.name])
 
         if df_non_missing.empty:
             raise ValueError("No non-missing values in the input data")
@@ -169,7 +187,8 @@ class DataPreprocessor(object):
 
         # Assign the trained classifier as an attribute
         return svm_classifier
-    
+
+
     def fill_missing_values_classifier(self, df, feature_columns, target_column, classifier):
         # Predict missing 'Season' values using the trained SVM classifier
         df_missing = df[df[target_column].isnull()]
@@ -195,10 +214,10 @@ class DataPreprocessor(object):
     def get_age_group_mapper(self):
         # Define a mapping dictionary
         age_group_mapping = {
-            'Middle Aged': 0,
-            'Adult': 1,
+            'Middle Aged': 4,
+            'Adult': 3,
             'Young Adult': 2,
-            'Senior': 3
+            'Senior': 1
         }
         
         # Define a function for mapping age groups to numbers
@@ -206,7 +225,21 @@ class DataPreprocessor(object):
             return age_group_mapping.get(age_group, age_group)  # Return the mapped value or the original value if not found
     
         return map_age_group
+    
+
+    def get_drinker_or_smoker_mapper(self):
+        # Define a mapping dictionary
+        drinker_or_smoker_mapping = {
+            'No': -1,
+            'Yes': 1,
+        }
         
+        # Define a function for mapping age groups to numbers
+        def map_yes_or_no(yes_no):
+            return drinker_or_smoker_mapping.get(yes_no, yes_no)  # Return the mapped value or the original value if not found
+    
+        return map_yes_or_no
+     
         
     def fit(self, features, labels):
 
@@ -226,9 +259,17 @@ class DataPreprocessor(object):
         """
         # Handling missing values
         #create regressors for the missing values that we found that have correlation
-        self._resident_distance_regressor = self.create_linear_regressor(features['Transportation expense'], features['Residence Distance'])
-        self._season_classifier = self.create_SVM_classifier(features['Month'], features['Season'], kernel='rbf') # Utilizing a robust model due to the cyclic nature of months and seasons
-        self._age_mapper = self.get_age_group_mapper()
+        self._Service_time = features['Service time'].mode().iloc[0]
+        self._height = features['Height'].mode().iloc[0]
+        self._age_group = features['Age Group'].map(self._age_mapper).mode().iloc[0]
+        self._pet = features["Pet"].mean()
+        self._son = features["Son"].median()
+
+        # init regressors and classifiers
+        self._resident_distance_regressor = self.create_linear_regressor(features[['Transportation expense', 'Height']], features['Residence Distance'])
+        self._season_classifier = self.create_SVM_classifier(features['Month'], features['Season'], kernel='rbf') 
+        self._weight_regressor = self.create_linear_regressor(features[['Height', 'Service time']], features['Weight'])
+
 
     def transform(self, df):
 
@@ -247,46 +288,119 @@ class DataPreprocessor(object):
 
         """
         transformed_df = df
-        # Apply the mapper function to the "Age Group" column
+        # Apply the mapper functions
         transformed_df['Age Group'] = transformed_df['Age Group'].map(self._age_mapper)
+        transformed_df['Smoker'] = transformed_df['Smoker'].map(self._yes_no_mapper)
+        transformed_df['Drinker'] = transformed_df['Drinker'].map(self._yes_no_mapper)
+        # Fill Nan with median value
+        transformed_df['Smoker'] = transformed_df['Smoker'].fillna(1)
+        transformed_df['Drinker'] = transformed_df['Drinker'].fillna(1)
+        transformed_df['Service time'] = transformed_df['Service time'].fillna(self._Service_time)
+        transformed_df['Height'] = transformed_df['Height'].fillna(self._height)
+        transformed_df['Age Group'] = transformed_df['Age Group'].fillna(self._age_group)
+        transformed_df['Pet'] = transformed_df['Pet'].fillna(self._pet)
+        transformed_df['Son'] = transformed_df['Son'].fillna(self._son)
 
-        df_dummies = pd.get_dummies(transformed_df['Reason'], prefix='Reason')
-        # Concatenate the new dummy variables with the original DataFrame
-        transformed_df = pd.concat([transformed_df, df_dummies], axis=1)
-        transformed_df.drop('Reason', axis=1, inplace=True)
-        
-        # Predict missing 'resident_distance' values using the trained regressor
-        transformed_df = self.fill_missing_values_regression(transformed_df, ['Transportation expense'], 'Residence Distance', self._resident_distance_regressor)
-
+        # # Predict missing 'resident_distance' values using the trained regressor
+        transformed_df = self.fill_missing_values_regression(transformed_df, ['Transportation expense', 'Height'], 'Residence Distance', self._resident_distance_regressor)
+        transformed_df = self.fill_missing_values_regression(transformed_df, ['Height', 'Service time'], 'Weight', self._weight_regressor) 
         transformed_df = self.fill_missing_values_classifier(transformed_df, ['Month'], 'Season', self._season_classifier)
 
-        print(transformed_df.info())
-        transformed_df['Smoker'].fillna('Yes')
+        categories_reason = [np.arange(0, 29)]  # Adjust this if your 'Reason' range is different
+        encoder_reason = OneHotEncoder(categories=categories_reason, sparse=False)
+        encoded_reason = encoder_reason.fit_transform(transformed_df[['Reason']])
+        encoded_reason_df = pd.DataFrame(encoded_reason, columns=[f"Reason_{i}" for i in range(0, 29)], index=transformed_df.index)
+        transformed_df = pd.concat([transformed_df.drop('Reason', axis=1), encoded_reason_df], axis=1)
 
-        # df["BMI"] = df['w'] / (df['h'] / 100) ** 2
-        # # think about if you would like to add additional computed columns.
+        categories_season = [np.arange(1, 5)]  # Adjust this if your 'Month' range is different (e.g., [np.arange(1, 13)] for 1-12)
+        encoder_season = OneHotEncoder(categories=categories_season, sparse=False)
+        encoded_season = encoder_season.fit_transform(transformed_df[['Season']])
+        encoded_season_df = pd.DataFrame(encoded_season, columns=[f"Season{i}" for i in range(1, 5)], index=transformed_df.index)  # Adjust the range if necessary
+        transformed_df = pd.concat([transformed_df.drop('Season', axis=1), encoded_season_df], axis=1) 
+
+
+        transformed_df['BMI'] = transformed_df['Weight'] / ((transformed_df['Height'] / 100) ** 2)
+
+        # Drop columns
+        transformed_df.drop(['Weight', 'Height', 'Education', 'ID', 'Month', 'Day'], axis=1, inplace=True)
 
         return transformed_df
 
 
+    def TimeOff_maper(self):
+        # Define a mapping dictionary
+        time_off_mapping = {
+            'Very High': 4,
+            'High': 3,
+            'Medium': 2,
+            'Low': 1
+        }
+        
+        # Define a function for mapping age groups to numbers
+        def time_off_group(time_group):
+            return time_off_mapping.get(time_group, time_group)  # Return the mapped value or the original value if not found
+    
+        return time_off_group
+
+
+    def clean_data(self, df):
+        df['TimeOff'] = df['TimeOff'].map(self._time_off_mapper)
+
+
+class ModelSelector(BaseEstimator, ClassifierMixin):
+    def __init__(self, estimator=SVC()):
+        self.estimator = estimator
+
+    def fit(self, X, y=None, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+    def predict(self, X, y=None):
+        return self.estimator.predict(X)
+
+    def score(self, X, y):
+        return self.estimator.score(X, y)
+
+
 def train_model(processed_X, y):
     """
-    This function gets the data after the pre-processing stage  - after running DataPreprocessor.transform on it, 
-    a vector of labels, and returns a trained model. 
-
-    Input:
-    processed_X (ndarray or dataframe): the data after the pre-processing stage
-    y: a vector of labels
-
-    Output:
-    model: an object with a "predict" method, which accepts the ***pre-processed*** data and outputs the prediction
-
-
+    Performs grid search to find the best combination of model, feature selection, and model parameters.
+    Parameters:
+    processed_X (DataFrame or ndarray): Feature dataset for training.
+    y (Series or ndarray): Target labels for training.
+    Returns:
+    best_model: The model with the best parameters found.
     """
-    model = RandomForestClassifier()
-    model.fit(processed_X, y)
+    # I tried a lot of model and the logistic regression give the best results
+    # Define the pipeline steps
+    pipe = Pipeline([
+        ('feature_selection', RFECV(estimator=SVC(kernel="linear"))),
+        ('model', ModelSelector())
+    ])
 
-    return model
+    param_grid = {
+        'feature_selection__estimator': [LogisticRegression(solver='liblinear')],
+        'model__estimator': [LogisticRegression(solver='liblinear')],
+        'model__estimator__C': [5, 10, 20, 40],
+        'model__estimator__penalty': ['l1'],
+        'model__estimator__fit_intercept': [True], 
+        'model__estimator__max_iter': [80],  
+        'model__estimator__tol': [0.01],  
+        'model__estimator__random_state': [42],  
+    }
+
+
+    # Initialize GridSearchCV
+    grid_search = GridSearchCV(pipe, param_grid, cv=5, n_jobs=-1)
+
+    # Perform grid search
+    grid_search.fit(processed_X, y)
+
+    # Print best parameters
+    print(f"Best Parameters: {grid_search.best_params_}")
+
+    # Return the best model
+    return grid_search.best_estimator_
 
 
 if __name__ == '__main__':
@@ -296,26 +410,25 @@ if __name__ == '__main__':
 
     X_train = train_dataset_df.iloc[:, :-1]
     y_train = train_dataset_df['TimeOff']
-    preprocessor.data_visualing(X_train)
     preprocessor.fit(X_train, y_train)
-    X_train = preprocessor.transform(X_train)
-    preprocessor.data_visualing(X_train)
-    # model = train_model(preprocessor.transform(X_train), y_train)
+    model = train_model(preprocessor.transform(X_train), y_train)
 
-    # ### Evaluation Section ####
-    # test_csv_path = 'time_off_data_train.csv'
-    # # Obviously, this will be different during evaluation. For now, you can keep it to validate proper execution
-    # test_csv_path = train_csv_path
-    # test_dataset_df = load_dataset(test_csv_path)
+    ### Evaluation Section ####
+    test_csv_path = 'real_time_off_data_train.csv'
+    # Obviously, this will be different during evaluation. For now, you can keep it to validate proper execution
+    test_csv_path = train_csv_path
+    test_dataset_df = load_dataset(test_csv_path)
 
-    # X_test = test_dataset_df.iloc[:, :-1]
-    # y_test = test_dataset_df['TimeOff']
+    X_test = test_dataset_df.iloc[:, :-1]
+    y_test = test_dataset_df['TimeOff']
 
-    # processed_X_test = preprocessor.transform(X_test)
-    # predictions = model.predict(processed_X_test)
-    # test_score = accuracy_score(y_test, predictions)
-    # print("Accuracy on test:", test_score)
+    processed_X_test = preprocessor.transform(X_test)
+    predictions = model.predict(processed_X_test)
+    test_score = accuracy_score(y_test, predictions)
 
-    # predictions = model.predict(preprocessor.transform(X_train))
-    # train_score = accuracy_score(y_train, predictions)
-    # print('Accuracy on train:', train_score)
+    predictions = model.predict(preprocessor.transform(X_train))
+    train_score = accuracy_score(y_train, predictions)
+
+    print("<==============================================>")
+    print("Accuracy on test:", test_score)
+    print('Accuracy on train:', train_score)
